9bb72024401c33d7b56f86a2aed62c44d62cfcba
Rosny Ratsianompo <rosny.rat@onja.org>
2020-09-14 07:40:33 +0100

    deleted .git ignore and restart installing the node sass

---
+++ script.js
@@ -0,0 +1,19 @@
+{
+    "id": "1fbef8c1-9823-4cb5-b138-259fed0fb2b1",
+    "lastName": "Kihn",
+    "firstName": "Effie",
+    "picture": "https://s3.amazonaws.com/uifaces/faces/twitter/jpenico/128.jpg",
+    "birthday": 92614674399
+}, {
+    "id": "567084f7-b7e8-41ba-90cb-be5ff8d78b68",
+    "lastName": "Spinka",
+    "firstName": "Adriel",
+    "picture": "https://s3.amazonaws.com/uifaces/faces/twitter/balintorosz/128.jpg",
+    "birthday": 1093495104944
+}, {
+    "id": "35194943-2f14-4066-89c7-e3101ad5bde1",
+    "lastName": "Schaefer",
+    "firstName": "Ricky",
+    "picture": "https://s3.amazonaws.com/uifaces/faces/twitter/stefanotirloni/128.jpg",
+    "birthday": 117168543755
+},
