0133178bfcee8f05456ca21846ac31100fb18a44
Rosny Ratsianompo <rosny.rat@onja.org>
2020-09-16 16:28:30 +0100

    edited the script.js

--- script.js
+++ script.js
@@ -1,168 +1,166 @@
-// import differenceInYears from 'date-fns/difference_in_years';
-
-// fetching the data from people.json
-const dataList = `people.json`;
-const container = document.querySelector(".container");
-
-async function fetchData() {
-    const response = await fetch(dataList);
-    const data = await response.json();
-    return data;
-}
-
-async function populateData() {
-    const peoples = await fetchData();
-    const sortedData = peoples.sort((a, b) => b.rt_score - a.rt_score);
-    const html = sortedData.map(data => {
-        return `
-            <div class='list-of-data'>
-                <image src="${data.picture}">
-                <h3>${data.firstName}</h3>
-                <p class="lastName">${data.lastName}</p>
-                <p>${data.birthday}</p>
-                
-                <button value="${data.id}"class="edit">
-                ✏ 
-                </button>
-                <button value="${data.id}" class="delete">
-                ||| 
-                </button>
-            </div>
-        `;
-    }).join('');
-    container.innerHTML = html;
-}
-populateData();
-
 function wait(ms = 0) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
-async function destroyShowList(showList) {
-    showList.classList.remove("open");
-    // Wait for 1 sec, to let the animation do its work.
-    await wait(1000);
-    // remove it from the DOM
-    showList.remove();
-    // remove it from the javascript memory
-    showList = null;
-}
+// fetching the data from people.json
+const dataList = `people.json`;
+const tbody = document.querySelector('tbody');
 
-// Here is the code that handle editForm, If the user want to edit the form
-const editForm = (e) => {
-    e.preventDefault();
-    if (e.target.closest('button.edit')) {
-        // every time we click the icon in each rows, the showList will appear  
-        const editButton = e.target.closest('.container');
-        const editedBtn = editButton.value;
-        editTheForm(editedBtn);
+// Function that fetch the data from people.json
+async function fetchData() {
+    const response = await fetch(dataList);
+    const data = await response.json();
+    let people = data;
+    console.log(people);
+
+    function displayList() {
+        const sortedData = people.sort((a, b) => b.birthday - a.birthday);
+        console.log(sortedData);
+        const html = people.map(
+            data => {
+                const dateNow = new Date();
+                const currentDate = new Date(data.birthday);
+                const day = currentDate.getDay();
+                const month = currentDate.getMonth();
+                const year = currentDate.getFullYear();
+                const fullDate = `${day} / ${month} / ${year}`;
+                const peopleBirth = dateNow.getFullYear() - currentDate.getFullYear();
+                return `
+                <tr class='list-of-data' data-id="${data.id}">
+                    <td><image src="${data.picture}" alt="${data.firstName + ' ' + data.lastName}"/></td>
+                    <td>${data.firstName}</td>
+                    <td class="lastName">${data.lastName}</td>
+                    <td>${peopleBirth}</td>
+                    <td>
+                        <button value="${data.id}"class="edit">
+                        ✏ 
+                        </button>
+                    </td>
+                    <td>
+                        <button value="${data.id}" class="delete">
+                        ||| 
+                        </button>
+                    </td>
+                </tr>
+            `;
+            }).join('');
+        tbody.innerHTML = html;
     }
-};
+    displayList();
 
-async function editTheForm(dataId) {
-    const formToEdit = await fetchData(dataList);
-    console.log(formToEdit)
+    // Destroy the popup
+    async function destroyPopup(popup) {
+        popup.classList.remove("open");
+        await wait(1000); // Wait for 1 sec
+        popup.remove(); // remove it from the DOM
+        popup = null; // remove it from the javascript memory
+    }
 
-    const editedForm = formToEdit.find(data => data.id !== dataId);
-    console.log(editedForm);
-
-    return new Promise(async function(resolve) {
-        // We create form here
-        const showList = document.createElement('form');
-        showList.classList.add('show-list');
-        showList.insertAdjacentHTML('afterbegin',
-            `
-            <div class="show-list">
+    // function that handle edit button and delete button
+    const handleClick = e => {
+        if (e.target.closest('button.edit')) {
+            const editButton = e.target.closest('tr');
+            const editedBtn = editButton.value;
+            editPerson(editedBtn);
+        }
+        if (e.target.closest('button.delete')) {
+            const deleteData = e.target.closest('tr');
+            const deleteB = deleteData.querySelector('button.delete');
+            console.log(deleteData);
+            const deleteBtn = deleteB.value;
+            deleteDataForm(deleteBtn);
+            console.log(deleteBtn);
+        }
+    };
+    // Function for editing the form here
+    function editPerson(dataId) {
+        const findPerson = people.find(person => person.id !== dataId);
+        return new Promise(async function(resolve) {
+            // We create form here
+            const popup = document.createElement('form');
+            popup.classList.add('popup');
+            popup.insertAdjacentHTML('afterbegin',
+                `
+            <div class="popup">
                 <label for="picture">Picture</label>
-				<input type="url" name="picture" value="${editedForm.picture}">
+				<input type="url" name="picture" value="${findPerson.picture}">
 				<label for="last-name">Last name</label>
-				<input type="text" name="lastName" value="${editedForm.lastName}">
+				<input type="text" name="lastName" value="${findPerson.lastName}">
 				<label for="first-name">First name</label>
-				<input type="text" name="firstName" value="${editedForm.firstName}">
+				<input type="text" name="firstName" value="${findPerson.firstName}">
 				<label for="birthday">Birthday</label>
-				<input type="text" name="birthday" value="${editedForm.birthday}">
+				<input type="text" name="birthday" value="${findPerson.birthday}">
 			</div>
 			<div class="buttons">
 				<button type="cancel" class="btn cancel">Cancel</button>
 				<button type="submit" class="btn submit">Save</button>
 			</div>
     	`
-        );
+            );
+            window.addEventListener('click', e => {
+                if (e.target.closest('button.cancel')) {
+                    destroyPopup(popup);
+                }
+            })
 
-        window.addEventListener('click', e => {
-            if (e.target.closest('button.cancel')) {
-                destroyShowList(showList);
-            }
-        })
+            popup.addEventListener(
+                'submit',
+                e => {
+                    e.preventDefault();
+                    findPerson.picture = e.target.picture.value,
+                        findPerson.lastName = e.target.lastName.value,
+                        findPerson.firstName = e.target.firstName.value,
+                        findPerson.birthday = e.target.birthday.value,
 
-        showList.addEventListener(
-            'submit',
-            e => {
-                e.preventDefault();
-                editedForm.picture = e.target.picture.value,
-                    editedForm.lastName = e.target.lastName.value,
-                    editedForm.firstName = e.target.firstName.value,
-                    editedForm.birthday = e.target.birthday.value,
-                    populateData(editedForm);
-                resolve(e.target.remove());
-                console.log(editedForm)
-                destroyShowList(showList);
-            }, { once: true });
-        document.body.appendChild(showList);
-        showList.classList.add('open');
-    });
-}
-
-// Code that handle the delete list
-const deleteForm = (e) => {
-    e.preventDefault();
-    // code delete function here
-    if (e.target.closest('button.delete')) {
-        const deleteData = e.target.closest('.container');
-        const deleteX = deleteData.querySelector('button.delete');
-        const deleteBtn = deleteX.id;
-        deleteDataForm(deleteBtn);
+                        displayList(findPerson);
+                    console.log(findPerson);
+                    resolve(e.target.remove());
+                    destroyPopup(popup);
+                }, { once: true });
+            document.body.appendChild(popup);
+            popup.classList.add('open');
+        });
     }
-};
 
-async function deleteDataForm(idToDelete) {
-    const findData = await fetchData();
-    const deleteButton = findData.filter(el => el.id === idToDelete);
-    console.log(deleteButton);
-    return new Promise(async function(resolve) {
-        const dataToDelete = document.createElement('div');
-        const lastName = document.querySelector('.lastName').textContent;
-        dataToDelete.classList.add('to-delete');
-        dataToDelete.insertAdjacentHTML('afterbegin',
-            `
+    // function for deleting item here
+    const deleteDataForm = (idToDelete) => {
+        // const deleteButton = people.filter(el => el.id !== idToDelete);
+        console.log(idToDelete);
+        return new Promise(async function(resolve) {
+            const lastName = document.querySelector('.lastName').textContent;
+
+            const dataToDelete = document.createElement('div');
+            dataToDelete.classList.add('to-delete');
+            dataToDelete.insertAdjacentHTML('afterbegin',
+                `
             <div class="to-deleteEl">
-                <p> Are you sure to remove <br> ${lastName}?</p>
+                <p> ⚠ </p>
+                <p> Do you want to remove <br> <q>${lastName}</q> from the list?
+                </p>
                 <button class="remove">Yes</button>
                 <button type="cancel" class="cancel">No</button>
 			</div>
         `);
 
-        dataToDelete.addEventListener('click', e => {
-            if (e.target.closest('button.cancel')) {
-                destroyShowList(dataToDelete);
-            }
+            window.addEventListener('click', e => {
+                if (e.target.closest('button.cancel')) {
+                    destroyPopup(dataToDelete);
+                }
+            });
+
+            dataToDelete.addEventListener('click', e => {
+                if (e.target.closest('button.remove')) {
+                    console.log(idToDelete);
+                    const removeData = people.filter(el => el.id !== idToDelete);
+                    const deleteFindData = removeData;
+                    people = deleteFindData;
+                    displayList(deleteFindData);
+                    destroyPopup(dataToDelete);
+                }
+            })
+            document.body.appendChild(dataToDelete);
+            dataToDelete.classList.add('open');
         });
-
-        dataToDelete.addEventListener('click', e => {
-            if (e.target.closest('button.remove')) {
-                const removeData = findData.filter(el => el.id !== idToDelete);
-                populateData(removeData);
-                destroyShowList(dataToDelete);
-            }
-        })
-        document.body.appendChild(dataToDelete);
-        dataToDelete.classList.add('open');
-    });
+    }
+    tbody.addEventListener('click', handleClick)
 }
-container.addEventListener('click', deleteForm);
-
-container.addEventListener('click', editTheForm);
-
-populateData(dataList)
-
-
-// handling date computation
+fetchData();
