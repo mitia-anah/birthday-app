eb41a8ccc6363a6162baa70b5ae5569590354003
Rosny Ratsianompo <rosny.rat@onja.org>
2021-03-15 15:42:31 +0300

    updated form

--- dist/birthday.4fc5c73a.js
+++ dist/birthday.4fc5c73a.js
@@ -469,7 +469,7 @@ async function fetchData() {
 }
 
 fetchData();
-},{"./element.js":"component/element.js","./destroyPopup.js":"component/destroyPopup.js","./generate.js":"component/generate.js"}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./element.js":"component/element.js","./destroyPopup.js":"component/destroyPopup.js","./generate.js":"component/generate.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -497,7 +497,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64222" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51415" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -673,5 +673,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","component/birthday.js"], null)
+},{}]},{},["../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","component/birthday.js"], null)
 //# sourceMappingURL=/birthday.4fc5c73a.js.map
