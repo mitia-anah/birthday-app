ec270f3f415765de5e53cce9fd4fc84c405e9943
Rosny Ratsianompo <rosny.rat@onja.org>
2021-03-27 14:23:24 +0300

    last update

--- dist/birthday.4fc5c73a.js
+++ dist/birthday.4fc5c73a.js
@@ -18960,7 +18960,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50250" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54805" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
